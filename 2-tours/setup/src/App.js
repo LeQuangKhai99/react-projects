8ac128d6cf2550fda9b9ce53f8325f037f146123
khailq-1062 <le.quang.khai@sun-asterisk.com>
2021-03-03 22:28:53 +0700

    3/3/2021

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -9,17 +9,29 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour)=> tour.id !== id);
+    setTours(newTours);
+  }
+
   const fetchTours = async () => {
     setLoading(true);
 
-    const response = await fetch(url);
-    const tours = await response.json();
-    console.log(tours);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error)
+    }
+  
   }
 
   useEffect(()=>{
     fetchTours();
-  });
+  }, []);
 
   if(loading) {
     return (
@@ -28,9 +40,19 @@ function App() {
       </main>
     );
   }
+  if(tours.length === 0){
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={() => fetchTours()}>Refresh</button>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
-      <Tour />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
