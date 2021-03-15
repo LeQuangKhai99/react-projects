3980ae36f0230f1e828dae4c5a35df7c775a6d7b
KhaiLQ2711 <khailq.icv@gmail.com>
2021-03-15 18:05:48 +0700

    18h05 15/3/2021

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import allReducers from './reducer';
+
+const store = createStore(allReducers);
 
 ReactDOM.render(
   <React.StrictMode>
