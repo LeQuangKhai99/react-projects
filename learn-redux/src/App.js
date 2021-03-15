9589737ce7ced0d97aefe434aed4c6ca58820f29
khailq-1062 <le.quang.khai@sun-asterisk.com>
2021-03-15 23:26:28 +0700

    23h26 15/3/2021

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,8 +1,18 @@
-
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from './actions';
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-     
+      <h1>Couter {counter}</h1>
+      <button onClick={() => {dispatch(increment(2))}}>+</button>
+      <button onClick={() => {dispatch(decrement(2))}}>-</button>
+      {isLogged ? <h3>Valueable Information I Shouldn't See </h3> : ''}
     </div>
   );
 }
